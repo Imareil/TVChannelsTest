6f7e8c3fe36f1f471a4350bc749e2e4767b69f75
Imariel <mail@fdmi3.ru>
2021-11-05 13:32:53 +0400

    Init

---
+++ TVChannelsTest/AppDelegate.swift
@@ -0,0 +1,7 @@
+// AppDelegate.swift
+// Copyright © Dmi3. All rights reserved.
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ TVChannelsTest/SceneDelegate.swift
@@ -0,0 +1,16 @@
+// SceneDelegate.swift
+// Copyright © Dmi3. All rights reserved.
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    }
+}
